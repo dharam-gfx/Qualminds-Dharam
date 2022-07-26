dd10dc70c551b7e21335fbba73e2badf562d637d
Dharmendra kumar <technicalradhe@gmail.com>
2022-07-26 16:46:36 +0530

    test3

---
+++ test/test3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 10;
+            double myDouble = 5.25;
+            bool myBool = true;
+
+            Console.WriteLine(Convert.ToString(myInt));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
+            Console.ReadLine();
+
+        }
+    }
+}
