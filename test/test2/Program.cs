a83e7fd39f47d17243b161ad502293c683c0750d
Dharmendra kumar <technicalradhe@gmail.com>
2022-07-26 16:44:06 +0530

    test2

---
+++ test/test2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 9;
+            double myDouble = myInt;       // Automatic casting: int to double
+
+            Console.WriteLine(myInt);      // Outputs 9
+            Console.WriteLine(myDouble);   // Outputs 9
+            Console.ReadLine();
+
+
+        }
+    }
+}
