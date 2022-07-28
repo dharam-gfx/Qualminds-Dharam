69e5028a64a659d3fc9e189797e24ac13d16d150
Dharmendra kumar <technicalradhe@gmail.com>
2022-07-28 18:59:15 +0530

    cc

---
+++ test/StringIntBool/StringIntBool/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringIntBool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = "44";
+            int result = 0;
+
+           bool check= int.TryParse(num, out result);
+            
+
+            if (check)
+            {
+                Console.WriteLine(result);
+
+            }
+            else
+            {
+                Console.WriteLine(num);
+            }
+            
+        }
+    }
+}
