e24f1e9b5b8f8d6416a619408f6dd248d9d5e58e
Dharmendra kumar <technicalradhe@gmail.com>
2022-07-29 15:42:52 +0530

    ang

---
+++ Anguler/MyFirstProject/src/app/button-click/button-click.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ButtonClickComponent } from './button-click.component';
+
+describe('ButtonClickComponent', () => {
+  let component: ButtonClickComponent;
+  let fixture: ComponentFixture<ButtonClickComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ButtonClickComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ButtonClickComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
