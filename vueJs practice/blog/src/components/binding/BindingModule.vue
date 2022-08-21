24411b719ffa250781f232937acea04319986c66
dharmendra kumar <technicalradhe@gmail.com>
2022-08-21 18:42:24 +0530

    class bind

--- vueJs practice/blog/src/components/binding/BindingModule.vue
+++ vueJs practice/blog/src/components/binding/BindingModule.vue
@@ -2,16 +2,18 @@
     <div>
         <h2>all binding html ,css </h2>
         <htmlBindingVue/>
+        <classBindingVue/>
     </div>
 </template>
 <script>
 
 import htmlBindingVue from './htmlBinding.vue';
-
+import classBindingVue from './classBinding.vue';
 export default {
     name: "BindingModule",
     components: {
         htmlBindingVue,
+        classBindingVue
     }
 }
 </script>
---
+++ vueJs practice/blog/src/components/binding/classBinding.vue
@@ -0,0 +1,55 @@
+<template >
+    <div>
+        <h2>class binding</h2>
+        <div :class="{ bg_green: flag }">
+            <h2>Dynamic class</h2>
+        </div>
+        <button type="button" v-on:click="flag = !flag">remove bg color</button>
+
+        <div :class="applymultiClass">
+            <h2>multi class binding</h2>
+        </div>
+        <div :class="applymultiClass">
+            <h2>multi class binding</h2>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            flag: true,
+        }
+    },
+    computed: {
+        applymultiClass() {
+            return {
+                tex_color: true,
+                text_size: true,
+                bg_color2: true,
+
+            }
+        }
+    }
+
+}
+</script>
+<style >
+.bg_green {
+    background-color: greenyellow;
+    padding: 5px 10px;
+}
+
+.tex_color {
+    color: blue;
+}
+
+.text_size {
+    font-size: 40px;
+}
+
+.bg_color2 {
+    background-color: aquamarine;
+    padding: 0px 25px;
+}
+</style>
